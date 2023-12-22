60ec4d1d80a223486bf7e83b380f1db328f790f9
Kennedy821 <57705754+Kennedy821@users.noreply.github.com>
2023-12-22 13:01:03 +0000

    Add files via upload

--- stp_beta_v1_soil_abs_added.py
+++ stp_beta_v1_soil_abs_added.py
@@ -241,7 +241,7 @@ with header:
         fig = plt.figure(figsize=(10,10))
         ax = plt.gca()
         ax.set_facecolor('black')
-        calplot.yearplot(events_tm, edgecolor='black', cmap='viridis_r', ax=ax, dropzero=True, fillcolor='whitesmoke', linecolor='black',linewidth=2.5)
+        calplot.yearplot(events_tm, edgecolor='black', cmap='autumn', ax=ax, dropzero=True, fillcolor='whitesmoke', linecolor='black',linewidth=1.5)
         plt.xticks(fontsize=8)
         plt.yticks(fontsize=8)
 
